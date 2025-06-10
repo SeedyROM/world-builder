afba3ea0d2ec406d957bd1f0bf01acf631b7e3d3
Zack Kollar <rallokkcaz@gmail.com>
2025-06-10 04:47:40 -0400

    :hammer: Use internal script for CI coverage task

--- scripts/test_runner.py
+++ scripts/test_runner.py
@@ -4,15 +4,28 @@ from pytest import main
 
 
 def test_without_coverage():
+    """Run tests without coverage."""
     sys.argv = ["pytest"] + sys.argv[1:]
     main()
 
 
 def test_with_coverage():
+    """Run tests with coverage and HTML report."""
     sys.argv = [
         "pytest",
         "--cov=world_builder",
         "--cov-report=html",
+        "--cov-report=term-missing",
+    ] + sys.argv[1:]
+    main()
+
+
+def test_with_ci_coverage():
+    """Run tests with coverage and XML report for CI."""
+    sys.argv = [
+        "pytest",
+        "--cov=world_builder",
+        "--cov-report=xml",
         "--cov-report=term-missing",
     ] + sys.argv[1:]
     main()
